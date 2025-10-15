32085986dfe5a295ac1d99050e6381c2010f0228
jlarata <ariel@cfee.gob.ar>
2025-10-15 09:15:45 -0300

    more developing of heroes component

--- angular-tour-of-heroes/src/app/heroes/heroes.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,8 +11,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './heroes.css'
 })
 export class Heroes {
-  hero: Hero = {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
+  
+/*   hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  };
+  }; */
+  
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
 }
